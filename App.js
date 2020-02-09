cb5a1f1c7564d9bcfbb96640d06c65fb537e6041
AndreFelipeGomes <andrefelipe_bg@outlook.com>
2020-02-09 13:18:44 -0300

    Add Date Picker and menu Top

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, AsyncStorage, ScrollView } from 'react-native';
-import { TextInput, Button, IconButton, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { TextInput, Button, DefaultTheme, Provider as PaperProvider, Appbar } from 'react-native-paper';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 export default class Main extends Component {
 
   state = {
     ArrayObject: [],
     textInput: '',
+    date: '',
+    isDatePickerVisible: false,
   }
   componentDidMount = () => {
+    var today = new Date();
+    today = today.toISOString()
+    var ano = today.substr(0,4)
+    var mes = today.substr(5,2)
+    var dia = today.substr(8,2)
     AsyncStorage.getItem('@ArrayObject', (err, result) => {
       this.setState({
-        ArrayObject: JSON.parse(result)
+        ArrayObject: JSON.parse(result),
+        date: dia + '/' + mes + '/' + ano
       })
     });
   }
@@ -54,22 +63,52 @@ export default class Main extends Component {
   }
 
   render() {
+    const showDatePicker = () => {
+      this.setState({isDatePickerVisible: true});
+    };
+    const hideDatePicker = () => {
+      this.setState({isDatePickerVisible: false});
+    };
+    const handleConfirm = date => {
+      var today = JSON.stringify(date);
+      var ano = today.substr(1,4)
+      var mes = today.substr(6,2)
+      var dia = today.substr(9,2)
+      hideDatePicker();
+      this.setState({date: dia + '/' + mes + '/' + ano});
+    };
     return (
       <PaperProvider theme={theme}>
-        <View style={{ flex: 1, backgroundColor: '#222222' }}>
+        <View style={{ flex: 1, backgroundColor: '#222222', }}>
+          <Appbar.Header>
+            <Appbar.Content
+              title="To do list"
+              subtitle={this.state.date}
+            />
+            <Appbar.Action icon="calendar" onPress={showDatePicker} />
+          </Appbar.Header>
+          <DateTimePickerModal
+            isVisible={this.state.isDatePickerVisible}
+            mode="date"
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+            isDarkModeEnabled={true}
+          />
+          <View style={{ paddingTop: 25 }}>
             <TextInput
               style={styles.textInput}
               label='Descrição'
               mode='outlined'
               value={this.state.textInput}
               onChangeText={value => this.setState({ textInput: value })}
-              onEndEditing={this._storageSave}
             />
+            <Button color='green' onPress={() => this._storageSave()}>Adicionar</Button>
+          </View>
           <ScrollView>
             {
               (this.state.ArrayObject) ?
                 this.state.ArrayObject.map((value, index) =>
-                  <View key={index} style={(!value.check) ? styles.box : styles.boxCheck }>
+                  <View key={index} style={(!value.check) ? styles.box : styles.boxCheck}>
                     <Text style={{ textAlign: 'center', color: 'white' }} key={index}>{value.text}</Text>
                     {
                       (!value.check) ?
@@ -86,7 +125,7 @@ export default class Main extends Component {
                 null
             }
           </ScrollView>
-          <Button onPress={() => this._limparArrayStorage()}>Limpar</Button>
+          <Button color='red' onPress={() => this._limparArrayStorage()}>Limpar</Button>
         </View>
       </PaperProvider>
     );
@@ -103,7 +142,7 @@ const styles = StyleSheet.create({
     marginRight: 14,
     borderColor: 'orange'
   },
-  boxCheck :{
+  boxCheck: {
     borderRadius: 10,
     borderWidth: 2,
     flex: 1,
@@ -114,8 +153,6 @@ const styles = StyleSheet.create({
   },
   textInput: {
     backgroundColor: '#222222',
-    marginTop: 52,
-    marginBottom: 2,
     marginLeft: 12,
     marginRight: 12,
   },
